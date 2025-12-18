5b0adecbab39b5ae9379769be3368266264d1f3b
Bùi Đạt-1802 <buixuandat1802@gmail.com>
2025-12-18 11:54:27 +0700

    fix: port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3001;
 // Middleware 
 app.use(cors());
 app.use(express.json());
@@ -120,6 +119,5 @@ app.delete("/api/users/:id", async (req, res) => {
 });
 
 // Start server 
-app.listen(PORT, () => {
-    console.log("Server running on http://localhost:3001");
-}); 
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
